2ed734d1c267c5065daf89550f878520bc75252b
Giorgio <giorgio@MacBook-Air-di-Giorgio.local>
2022-11-09 08:45:50 +0100

    validation user

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './RegPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RedPage from './Redirection'
 
 import API from './API';
 
@@ -11,7 +12,6 @@ function App() {
 
   const addUser = (email,password,role)=>{
       const user = {email,password,role}
-      //console.log(user); 
       //API.addUser()
   }
  
@@ -21,9 +21,10 @@ function App() {
   
     <BrowserRouter>
       <Routes>
-        
+
           <Route path='/' element={ <SignIn addUser={addUser}/>} />
-        
+          <Route path='/redirect/' element={ <RedPage />} />
+          
         </Routes>
     </BrowserRouter>
    
