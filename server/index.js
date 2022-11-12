1e83f8015b397b7a791d9a305ffa5ca643a133ee
Cristina Biasi <s281936@studenti.polito.it>
2022-11-12 17:10:14 +0100

    S202207HT-48 Add APIs and dao methods for HT-2

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const dao = require("./dao");
+const fileUpload = require("express-fileupload");
 
 const bodyParser = require('body-parser'); // parser middleware
 const session = require('express-session');  // session middleware
@@ -74,7 +75,7 @@ const corsOptions = {
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
-
+app.use(fileUpload());
 
 
 app.use(session({
@@ -316,6 +317,82 @@ app.get('/api/validate/:code', async (req, res) => {
 		return res.status(500).json({error: err})
 	}
 })
+
+// FILE UPLOAD(.gpx*)
+app.post('/upload', async(req, res) => {
+	if (req.files === null) {
+		return res.status(400).json({ msg: 'No file uploaded' });
+	}
+
+	const file = req.files.file;
+
+	file.mv(`../client/public/uploads/${file.name}`, err => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send(err);
+		}
+
+		res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+	});
+});
+/*
+app.get(`${__dirname}/upload`, (req,res)=> {
+    res.download("./uploads/location.gpx");
+});
+*/
+
+/** Create Hike APIs **/
+
+app.get('/api/hiking', async (req, res) => {
+	try {
+		const hike = await dao.getHike();
+		return res.status(200).json(hike);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ error: err });
+
+	}
+});
+
+// hiking desc
+app.get("/api/hiking/:id", async (req, res) => {
+	try {
+		const hikedesc = await dao.getHikeDesc(req.params.id);
+		res.status(200).json(hikedesc);
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+//hiking post
+app.post('/api/hiking', isLoggedIn, isLocalGuide, async (req, res) => {
+	try {
+		const status = await dao.createHiking(req.body.title, req.body.length, req.body.description, req.body.difficulty, req.body.estimatedTime, req.body.ascent, req.body.localguideID);
+		if (status === '422')
+			res.status(422).json({ error: `Validation of request body failed` }).end();
+		else
+			return res.status(201).end();
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: `Generic error` }).end();
+	}
+})
+
+// hiking delete
+app.delete('/api/hiking/delete', async (req, res) => {
+	try {
+		const status = await dao.deleteHikes();
+		if (status === '404'){
+			console.log(status);
+			res.status(404).json({ error: `Validation of request body failed` }).end();
+		}
+		else{
+			return res.status(201).end();}
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
 
 /* -- SERVER ACTIVATION -- */
 app.listen(port, () => {
