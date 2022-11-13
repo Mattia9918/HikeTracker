d80026904f7f6b8c5552bc079f5960defaae9c87
Cristina Biasi <s281936@studenti.polito.it>
2022-11-13 22:51:57 +0100

    S202207HT-59 Test dao methods for user

--- server/unit_test/user.test.js
+++ server/unit_test/user.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const dao = require('../dao');
+const bcrypt = require("bcrypt");
 
 describe("test user", () => {
 
@@ -8,76 +9,91 @@ describe("test user", () => {
         await dao.deleteUser();
     })
 
-    testInsertUserOk();
-    testInsertUserErr();
+    testUserOk();
 });
 
-function testInsertUserOk() {
+function testUserOk() {
     const user = {
         name: "Mario",
         surname: "Rossi",
         username: "mariorossi1",
         email: "mario.rossi@mail.it",
         password: "password",
-        salt: "salt",
         role: "role"
     }
 
     test("Insert user OK", async () => {
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+
         let id = await dao.insertUser(
             user.email,
-            user.password,
-            user.salt,
+            hash,
+            salt,
             user.role,
             user.name,
             user.surname,
             user.username
         );
+
         let result = await dao.getUserById(id)
 
         expect(result.email).toEqual(user.email);
-        expect(result.hash).toEqual(user.password);
-        expect(result.salt).toEqual(user.salt);
+        expect(result.hash).toEqual(hash);
+        expect(result.salt).toEqual(salt);
         expect(result.role).toEqual(user.role);
         expect(result.name).toEqual(user.name);
         expect(result.surname).toEqual(user.surname);
         expect(result.username).toEqual(user.username);
         expect(result.isActive).toEqual(0);
+
+        let result2 = await dao.getUserByEmail(user.email)
+        expect(result2).toEqual(result)
+
+        let result3 = await dao.getUserByUsername(user.username)
+        expect(result3).toEqual(result)
+
+        await dao.activateUser(user.email)
+        let activeUser = await dao.getUserByEmail(user.email)
+        expect(activeUser.isActive).toEqual(1)
+
+        let login = await dao.getUserByCredentials(user.email, user.password)
+        expect(login).toEqual(activeUser)
+
+        let wrongCred = await dao.getUserByCredentials(user.email, "sbagliata")
+        expect(wrongCred).toBeFalsy()
     })
 }
-/*
-function testInsertUserErr() {
-    const user = {
-        name: "Mario",
-        surname: "Rossi",
-        username: "mariorossi1",
-        email: "mario.rossi@mail.it",
-        password: "password",
-        salt: "salt",
-        role: "role"
+
+describe("test activation", () => {
+
+    beforeEach(async () => {
+        await dao.deleteTableActivation();
+    })
+
+    testActivationOk();
+});
+
+function testActivationOk() {
+    const activation = {
+        email: "mario.rossi@gmail.com",
+        code: "code"
     }
 
-    test("Insert user Err", async () => {
-        await dao.insertUser(
-            user.email,
-            user.password,
-            user.salt,
-            user.role,
-            user.name,
-            user.surname,
-            user.username
+    test("Insert activation OK", async () => {
+        let code = await dao.insertActivation(
+            activation.email,
+            activation.code
         );
+        let result = await dao.getActivationByCode(code)
 
-        expect(await dao.insertUser(
-            user.email,
-            user.password,
-            user.salt,
-            user.role,
-            user.name,
-            user.surname,
-            user.username
-        )).toThrowError(SQLITE_CONSTRAINT)
+        expect(result.email).toEqual(activation.email);
+        expect(result.code).toEqual(activation.code);
+
+        await dao.deleteActivation(activation.email)
+        let undef = await dao.getActivationByCode(activation.code)
+        expect(undef).toBeUndefined()
     })
 }
- */
 
