192dd8040a65a4200e3750ad1a5a72e3b03bac29
gio4141 <100480700+gio4141@users.noreply.github.com>
2022-11-13 11:46:53 +0100

    S202207HT-60 E2E Testing

--- server/test/integrationTest.js
+++ server/test/integrationTest.js
@@ -1,73 +1,300 @@
+const { expect } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.should();
-
+const dao = require('../dao.js');
 const { app } = require('../index');
 var agent = chai.request.agent(app);
 
 
-describe('test register user api', () => {
 
 
-    beforeEach(async ()=>{
-        await agent.delete('/api/deleteUser');
-        await agent.delete('/api/deleteTableActivation');
+//REGISTER 
+
+//utente inserito con successo (200)
+describe('test api/register (case success 200)', () => {
+
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
     }); 
 
-    const userAdd = {
+    const user = {
         "email":"c@gmail.com",
         "role":"admin",
-        "password":"password"
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
     }; 
-    register(200, userAdd);
-   
+
+    it('test post /api/register', async () => {
+        await agent.post('/api/register')
+            .send(user)
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.should.have.property("code"); 
+                const code = res.body.code; 
+                dao.getActivationByCode(code).then(email=>{
+                    email.should.equal(user.email);
+                }); 
+               
+            }); 
+
+    })
     
 });
- 
+
+//formato dati utente non valido (email senza "@",...) (422)
+describe('test api/register (case invalid data 422)', () => {
+
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+    }); 
+
+    const user = {
+        "email":"cgmail.com",
+        "role":"admin",
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
+    }; 
+    register(422, user); 
+    
+});
+
+//utente già registrato (500)
+describe('test api/register (case user already defined 500)', () => {
+
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+    }); 
+
+    const user = {
+        "email":"c@gmail.com",
+        "role":"admin",
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
+    }; 
+    register(200, user);
+    register(500, user);
+    
+});
+
+//API REGISTER UTENTE
 function register(expectedHTTPStatus, user) {
     it('test post /api/register', async () => {
         await agent.post('/api/register')
             .send(user)
             .then(function (res) {
                 res.should.have.status(expectedHTTPStatus);
+                if(expectedHTTPStatus===422)
+                    res.text.should.have.string("Invalid value");
+                else if(expectedHTTPStatus===500) 
+                    res.text.should.have.string("SQLITE_CONSTRAINT");
             }); 
 
     })
 }
 
 
-describe('test validate user api', () => {
+//VALIDATE
+
+//utente registrato correttamente (200)
+describe('test api/validate/:code', async() => {
+
+    const user = {
+        "email":"c@gmail.com",
+        "role":"admin",
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
+    };
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+        
+    }); 
+    
+    validate(200, user);
+    
+    
+});
+
+//riga activation non presente (500)
+describe('test api/validate/:code', () => {
 
 
-    beforeEach(async ()=>{
-        await agent.delete('/api/deleteUser');
-        await agent.delete('/api/deleteTableActivation');
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+    }); 
+
+    validateByCode(500, "12345");
+    
+});
+
+
+//API VALIDATE = controllo utente registrato (200)
+function validate(expectedHTTPStatus,user) {
+    it('test post /api/validate/:code', async () => {
+        const code = await agent.post('/api/register')
+        .send(user)
+        .then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+            return res.body.code; 
+        });
+    
+        await agent.get('/api/validate/'+code)
+        .then(function(res){
+            res.should.have.status(expectedHTTPStatus);
+            const utente = res.body; 
+            utente.should.have.property("email"); 
+            utente.should.have.property("username"); 
+            utente.email.should.equal(user.email); 
+            utente.username.should.equal(user.username);  
+        })
+        ; 
+    })
+}
+
+//API VALIDATE = controllo riga activation rimasta (500)
+function validateByCode(expectedHTTPStatus, code) {
+    it('test post /api/validate/:code', async () => {
+        await agent.get('/api/validate/'+code)
+        .then(function(res){
+            res.should.have.status(expectedHTTPStatus);
+        })
+        ; 
+    })
+}
+
+
+
+//LOGIN 
+
+//utente login corretto (200)
+describe('test api/sessions 200', async() => {
+
+    const user = {
+        "email":"c@gmail.com",
+        "role":"admin",
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
+    }; 
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+        
+        const code = await agent.post('/api/register')
+        .send(user)
+        .then(function (res) {
+            return res.body.code; 
+        }); 
+        await agent.get('/api/validate/'+code); 
+
         
     }); 
 
+    it('test post /api/sessions', async () => {
+        
 
-    const userAdd = {
-        "email":"c@gmail.com",
-        "role":"admin",
-        "password":"password"
-    }; 
-    validate(200, userAdd);
+        await agent.post('/api/sessions') 
+        .send({username:user.email,password:user.password})
+        .then(function (res) {
+            res.should.have.status(200);
+            const utente = res.body; 
+            utente.email.should.equal(user.email); 
+            
+        
+        });  
+
+    })
     
     
 });
 
 
-function validate(expectedHTTPStatus, user) {
-    it('test post /api/validate/:code', async () => {
-        await agent.post('/api/register')
-            .send(user)
-            .then(function (res) {
-                res.should.have.status(expectedHTTPStatus);
-            }); 
-        const code = await agent.get('/api/getActivationByEmail')
-            .send({"email":"c@gmail.com",})
+//utente non registrato (401)
+describe('test api/sessions 401', async() => {
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation();
+        
+    }); 
+
+    login(401,{username:"d@gmail.com",password:"test"})
+    
+});
+
+//utente registrato password sbagliata (401)
+describe('test api/sessions 200', async() => {
+
+    const user = {
+        "email":"c@gmail.com",
+        "role":"admin",
+        "password":"password",
+        "name":"name",
+        "surname":"surname",
+        "username":"username",
+    }; 
+
+    before(async ()=>{
+        dao.deleteUser(); 
+        dao.deleteTableActivation(); 
+        
+        const code = await agent.post('/api/register')
+        .send(user)
+        .then(function (res) {
+            return res.body.code; 
+        }); 
         await agent.get('/api/validate/'+code); 
+
+        
+    }); 
+
+    it('test post /api/sessions', async () => {
+        
+
+        await agent.post('/api/sessions') 
+        .send({username:user.email,password:"1234"})
+        .then(function (res) {
+            res.should.have.status(401);
+            
+        
+        });  
+
+    })
+    
+    
+});
+
+
+//API LOGIN 
+function login(expectedHTTPStatus, user) {
+    it('test post /api/sessions', async () => {
+        
+
+        await agent.post('/api/sessions') 
+        .send({username:user.email,password:user.password})
+        .then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+        });  
+
     })
 }
-
