382577e7be5204daf0da979590a1a7fe0e424c40
Cihan Yurtsever <s296824@studenti.polito.it>
2022-11-23 16:12:47 +0100

    S202207HT-99 Back-end development

--- server/modules/routers/hikeRouter.js
+++ server/modules/routers/hikeRouter.js
@@ -131,4 +131,36 @@ router.delete('/api/points', async (req, res) => {
     }
 });
 
+//Parking lot
+router.post('/api/parking'
+, async (req, res) => {
+
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+
+        const parkID = await hike_dao.createParking(req.body.name, req.body.guarded, req.body.parking_spaces, req.body.price_per_hour, req.body.disabled_parkings, req.body.timetable);
+        
+        // const endingPointID = await hike_dao.postPoint(req.body.endingPoint);
+       
+        // await hike_dao.postHike_Point(hikeID, "arrive", endingPointID);
+
+        // for (let i in req.body.pointsOfInterest) {
+        //     let pointID = await hike_dao.postPoint(req.body.pointsOfInterest[i]);
+        //     await hike_dao.postHike_Point(hikeID, "interest", pointID);
+        // }
+        return res.status(201).json(parkID );
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: `Generic error` }).end();
+    }
+
+
+});
+
+
+
 module.exports = router;
