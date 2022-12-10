916e15eef8f109ba2afad417c153f53faab17b99
Lorenzo Ferro <lorenzoferro15@gmail.com>
2022-12-10 17:59:14 +0100

    S202207HT-126 Client-side code improvements

--- server/modules/routers/hutRouter.js
+++ server/modules/routers/hutRouter.js
@@ -2,201 +2,213 @@
 
 const express = require("express");
 const hut_dao = require("../dao/hutdao");
-const {check, validationResult} = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const hike_dao = require("../dao/hikedao");
 const checkAuth = require("../../authMiddleware");
 
 const router = express.Router();
 
-router.post('/api/hut', checkAuth.isLocalGuide,
-    []
-    , async (req, res) => {
+router.post(
+	"/api/hut",
+	checkAuth.isLocalGuide,
+	[
+		check("hut.email").isEmail(),
+		check("hut.website").isURL(),
+		check("hut.description").notEmpty(),
+		check("hut.altitude").isNumeric(),
+		check("hut.languages").isIn(["french", "english", "german"]),
+		check("hut.reachability").isIn(["foot", "cable", "normal", "offroad"]),
+		check("hut.rooms").isNumeric(),
+		check("hut.bathrooms").isNumeric(),
+		check("hut.beds").isNumeric(),
+		check("hut.bike_friendly").isIn([0, 1]),
+		check("hut.disabled_services").isIn([0, 1]),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+		try {
+			const pointId = await hike_dao.postPointHut(req.body.point);
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        try {
-            const pointId = await hike_dao.postPointHut(req.body.point);
+			const hutID = await hut_dao.postHut(req.body.hut, pointId);
 
-            const hutID = await hut_dao.postHut(req.body.hut, pointId);
-
-            return res.status(201).json({ "id": hutID });
-            
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ error: `Generic error` }).end();
-        }
-
-
-    })
+			return res.status(201).json({ id: hutID });
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({ error: `Generic error` }).end();
+		}
+	}
+);
 
 // Post the Hike_Point linked with Hut
-router.post('/api/hutLinkHike', checkAuth.isLocalGuide,
-[]
-, async (req, res) => {
+router.post(
+	"/api/hutLinkHike",
+	checkAuth.isLocalGuide,
+	[],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+		try {
+			const hut = await hut_dao.getHutById(req.body.hutId);
 
+			const hikeID = await hike_dao.postHike_Point(
+				req.body.hikeid,
+				"hut",
+				hut.point_id
+			);
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-    try {
-       
-        
-        const  hut   = await hut_dao.getHutById(req.body.hutId);
-        
-        const hikeID = await hike_dao.postHike_Point(req.body.hikeid, "hut", hut.point_id);
-
-        return res.status(201).json({ "id": hikeID });
-        
-    } catch (err) {
-        
-        res.status(500).json({ error: `Generic error` }).end();
-    }
-
-
-})
+			return res.status(201).json({ id: hikeID });
+		} catch (err) {
+			res.status(500).json({ error: `Generic error` }).end();
+		}
+	}
+);
 
 /** Get all huts **/
 router.get("/api/huts", checkAuth.isLoggedIn, async (req, res) => {
-
-    try {
-        const huts = await hut_dao.getHuts();
-        return res.status(200).json(huts);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: err });
-    }
+	try {
+		const huts = await hut_dao.getHuts();
+		return res.status(200).json(huts);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ error: err });
+	}
 });
 
 // hikeLinkedhut delete
-router.delete('/api/hut/deleteLink', async (req, res) => {
-    try {
-        await hut_dao.deleteHikeLinkedHut();
-        return res.status(204).end();
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
+router.delete("/api/hut/deleteLink", async (req, res) => {
+	try {
+		await hut_dao.deleteHikeLinkedHut();
+		return res.status(204).end();
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
 });
-
 
 /** Get hut by id **/
 router.get("/api/hut/:id", checkAuth.isLoggedIn, async (req, res) => {
-
-    try {
-        const id = req.params.id;
-        const hut = await hut_dao.getHutById(id);
-        if(hut === undefined) {
-            return res.status(404).json({error: "Hut not found!"})
-        }
-        return res.status(200).json(hut);
-    } catch (err) {
-        return res.status(500).end();
-    }
+	try {
+		const id = req.params.id;
+		const hut = await hut_dao.getHutById(id);
+		if (hut === undefined) {
+			return res.status(404).json({ error: "Hut not found!" });
+		}
+		return res.status(200).json(hut);
+	} catch (err) {
+		return res.status(500).end();
+	}
 });
 
 /** Get huts with filters **/
 router.get(`/api/hut*`, checkAuth.isLoggedIn, async (req, res) => {
-
-    try {
-        let huts;
-        const filter = req.query.filter;
-        switch (filter) {
-            case "none":
-                huts = await hut_dao.getHuts();
-                break;
-            case "altitude":
-                huts = await hut_dao.getHutByAltitude(req.query.value1, req.query.value2);
-                break;
-            case "restaurant_service":
-                huts = await hut_dao.getHutWithRestaurant()
-                break;
-            case "disabled_services":
-                huts = await hut_dao.getHutWithDisabledServices()
-                break;
-            case "bike_friendly":
-                huts = await hut_dao.getHutBikeFriendly()
-                break;
-            case "city":
-                huts = await hut_dao.getHutByCity(req.query.value1);
-                break;
-            case "province":
-                huts = await hut_dao.getHutByProvince(req.query.value1);
-                break;
-            case "beds":
-                huts = await hut_dao.getHutWithBeds();
-                break;
-            case "reach":
-                huts = await hut_dao.getHutByReachability(req.query.value1);
-                break;
-            case "area":
-                /* Gets huts that are in a rectangular area with diagonal from bottom-left to upper right */
-                huts = await hut_dao.getHutByArea(req.query.value1, req.query.value2);
-                break;
-            default:
-                console.log("wrong filter error");
-                return res.status(422).json({ error: `Validation of request body failed` }).end();
-        }
-        return res.status(200).json(huts);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: err });
-    }
+	try {
+		let huts;
+		const filter = req.query.filter;
+		switch (filter) {
+			case "none":
+				huts = await hut_dao.getHuts();
+				break;
+			case "altitude":
+				huts = await hut_dao.getHutByAltitude(
+					req.query.value1,
+					req.query.value2
+				);
+				break;
+			case "restaurant_service":
+				huts = await hut_dao.getHutWithRestaurant();
+				break;
+			case "disabled_services":
+				huts = await hut_dao.getHutWithDisabledServices();
+				break;
+			case "bike_friendly":
+				huts = await hut_dao.getHutBikeFriendly();
+				break;
+			case "city":
+				huts = await hut_dao.getHutByCity(req.query.value1);
+				break;
+			case "province":
+				huts = await hut_dao.getHutByProvince(req.query.value1);
+				break;
+			case "beds":
+				huts = await hut_dao.getHutWithBeds();
+				break;
+			case "reach":
+				huts = await hut_dao.getHutByReachability(req.query.value1);
+				break;
+			case "area":
+				/* Gets huts that are in a rectangular area with diagonal from bottom-left to upper right */
+				huts = await hut_dao.getHutByArea(
+					req.query.value1,
+					req.query.value2
+				);
+				break;
+			default:
+				console.log("wrong filter error");
+				return res
+					.status(422)
+					.json({ error: `Validation of request body failed` })
+					.end();
+		}
+		return res.status(200).json(huts);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ error: err });
+	}
 });
 
 router.get("/api/citiesHut", async (req, res) => {
-    try {
-        const cities = await hut_dao.getHutCities();
-        res.status(200).json(cities);
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
+	try {
+		const cities = await hut_dao.getHutCities();
+		res.status(200).json(cities);
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
 });
 
 router.get("/api/provincesHut", async (req, res) => {
-    try {
-        const provinces = await hut_dao.getHutProvinces();
-        res.status(200).json(provinces);
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
+	try {
+		const provinces = await hut_dao.getHutProvinces();
+		res.status(200).json(provinces);
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
 });
 
 router.get("/api/linkedHut", async (req, res) => {
-    
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-    try {
-        const linkedHut = await hut_dao.getHutsLinkedHike();
-        res.status(200).json(linkedHut);
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+	}
+	try {
+		const linkedHut = await hut_dao.getHutsLinkedHike();
+		res.status(200).json(linkedHut);
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
 });
 
 router.get("/api/linkedHut/:id", async (req, res) => {
-    
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-    
-    try {
-        const id = req.params.id;
-        const linkedHut = await hut_dao.getHutLinkedToHikeById(id);
-        res.status(200).json(linkedHut);
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+	}
+
+	try {
+		const id = req.params.id;
+		const linkedHut = await hut_dao.getHutLinkedToHikeById(id);
+		res.status(200).json(linkedHut);
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
 });
 
-
-module.exports = router; 
+module.exports = router;
--- server/test/testHut.js
+++ server/test/testHut.js
@@ -18,76 +18,74 @@ const localGuide = {
 	salt: "",
 	password: "password",
 	role: "localGuide",
-  };
+};
 
 const hut1 = {
-	"name": "Hut a Ivrea",
-	"address": "Via Ivrea",
-	"phone_number": "1111",
-	"email": "hut@mail.it",
-	"website": "www.hut.it",
-	"description": "un hut a Ivrea",
-	"altitude": 400,
-	"languages": "Inglese",
-	"bike_friendly": 0,
-	"reachability": "normal",
-	"disabled_services": 1,
-	"rooms": 10,
-	"bathrooms": 10,
-	"beds": 15,
-	"restaurant_services": 1
+	name: "Hut a Ivrea",
+	address: "Via Ivrea",
+	phone_number: "1111",
+	email: "hut@mail.it",
+	website: "www.hut.it",
+	description: "un hut a Ivrea",
+	altitude: 400,
+	languages: "english",
+	bike_friendly: 0,
+	reachability: "normal",
+	disabled_services: 1,
+	rooms: 10,
+	bathrooms: 10,
+	beds: 15,
+	restaurant_services: 1,
 };
 const point1 = {
-	"latitude": 45.459,
-	"longitude": 7.873,
-	"city": "Ivrea",
-	"province": "Torino", 
-}
+	latitude: 45.459,
+	longitude: 7.873,
+	city: "Ivrea",
+	province: "Torino",
+};
 const hut2 = {
-	"name": "Hut a Bra",
-	"address": "Via Bra",
-	"phone_number": "2222",
-	"email": "hut@mail.it",
-	"website": "www.hut.it",
-	"description": "un hut a Bra",
-	"altitude": 300,
-	"languages": "Francese",
-	"bike_friendly": 1,
-	"reachability": "foot",
-	"disabled_services": 1,
-	"rooms": 10,
-	"bathrooms": 10,
-	"beds": 15,
-	"restaurant_services": 1
+	name: "Hut a Bra",
+	address: "Via Bra",
+	phone_number: "2222",
+	email: "hut@mail.it",
+	website: "www.hut.it",
+	description: "un hut a Bra",
+	altitude: 300,
+	languages: "french",
+	bike_friendly: 1,
+	reachability: "foot",
+	disabled_services: 1,
+	rooms: 10,
+	bathrooms: 10,
+	beds: 15,
+	restaurant_services: 1,
 };
 const point2 = {
-	"latitude": 44.704,
-	"longitude": 7.8567,
-	"city": "Bra",
-	"province": "Cuneo",
-}
+	latitude: 44.704,
+	longitude: 7.8567,
+	city: "Bra",
+	province: "Cuneo",
+};
 
 async function deleteTables() {
 	await hike_dao.deletePoint();
 	await user_dao.deleteUser();
-	await hut_dao.deleteAllHuts(); 
-  
-  }
+	await hut_dao.deleteAllHuts();
+}
 
 async function insertUsers() {
-
 	localGuide.salt = await bcrypt.genSalt(10);
 	localGuide.hash = await bcrypt.hash(localGuide.password, localGuide.salt);
 
 	await user_dao.insertUser(localGuide);
 	await user_dao.activateUser(localGuide.email);
 }
-  
-  async function logUser(email, password) {
+
+async function logUser(email, password) {
 	await agent
-	  .post("/api/sessions")
-	  .send({ username: email, password: password });
-  }
+		.post("/api/sessions")
+		.send({ username: email, password: password });
+}
 
 describe("test api/huts (case success 200)", () => {
 	before(async () => {
@@ -97,14 +95,12 @@ describe("test api/huts (case success 200)", () => {
 		await insertUsers();
 
 		await logUser("mario.rossi@mail.it", "password");
-		
+
 		await agent.post("/api/hut").send({ hut: hut1, point: point1 });
-    	await agent.post("/api/hut").send({ hut: hut2, point: point2 });
+		await agent.post("/api/hut").send({ hut: hut2, point: point2 });
 	});
 
-
-
-    getHuts(200);
+	getHuts(200);
 	getHutsByAltitude(200, 300);
 	getHutsByResturantService(200);
 	getHutsByBikeFriendly(200);
@@ -113,84 +109,93 @@ describe("test api/huts (case success 200)", () => {
 	getHutsByProvince(200, "Cuneo");
 	getHutsByBeds(200, 2);
 	getHutsByReachability(200, "normal");
-
 });
 
 function getHuts(expectedHTTPStatus) {
 	it("test getHuts", async () => {
-
 		await agent.get("/api/huts").then(function (res) {
-            res.should.have.status(expectedHTTPStatus);
+			res.should.have.status(expectedHTTPStatus);
 			res.body.length.should.equal(2);
 		});
 	});
 }
 
 function getHutsByAltitude(expectedHTTPStatus, minALtitude) {
-	it('test get by altitude', async () => {
-
-			await agent.get(`/api/huts?filter=altitude&value1=${minALtitude}`).then( function (res) {
+	it("test get by altitude", async () => {
+		await agent
+			.get(`/api/huts?filter=altitude&value1=${minALtitude}`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByResturantService(expectedHTTPStatus) {
-	it('test get by altitude', async () => {
-			await agent.get(`/api/huts?filter=resturant_service&value1=true`).then( function (res) {
+	it("test get by altitude", async () => {
+		await agent
+			.get(`/api/huts?filter=restaurant_services&value1=1`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByDIsabledService(expectedHTTPStatus) {
-	it('test get by disabled service', async () => {
-		await agent.get(`/api/huts?filter=disabled_services&value1=true`).then( function (res) {
-			res.should.have.status(expectedHTTPStatus);
+	it("test get by disabled service", async () => {
+		await agent
+			.get(`/api/huts?filter=disabled_services&value1=1`)
+			.then(function (res) {
+				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByBikeFriendly(expectedHTTPStatus) {
-	it('test get by bike friendly', async () => {
-			await agent.get(`/api/huts?filter=bike_friendly&value1=true`).then( function (res) {
+	it("test get by bike friendly", async () => {
+		await agent
+			.get(`/api/huts?filter=bike_friendly&value1=1`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByCity(expectedHTTPStatus, city) {
-	it('test get by city', async () => {
-			await agent.get(`/api/huts?filter=city&value1=${city}`).then( function (res) {
+	it("test get by city", async () => {
+		await agent
+			.get(`/api/huts?filter=city&value1=${city}`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByProvince(expectedHTTPStatus, province) {
-	it('test get by province', async () => {
-			await agent.get(`/api/huts?filter=province&value1=${province}`).then( function (res) {
+	it("test get by province", async () => {
+		await agent
+			.get(`/api/huts?filter=province&value1=${province}`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByBeds(expectedHTTPStatus, minBeds) {
-	it('test get by beds', async () => {
-			await agent.get(`/api/huts?filter=beds&value1=${minBeds}`).then( function (res) {
+	it("test get by beds", async () => {
+		await agent
+			.get(`/api/huts?filter=beds&value1=${minBeds}`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
 
 function getHutsByReachability(expectedHTTPStatus, reach) {
-	it('test get by reachability', async () => {
-
-			await agent.get(`/api/huts?filter=reach&value1=${reach}`).then( function (res) {
+	it("test get by reachability", async () => {
+		await agent
+			.get(`/api/huts?filter=reach&value1=${reach}`)
+			.then(function (res) {
 				res.should.have.status(expectedHTTPStatus);
+			});
 	});
-});
 }
-
-
-
