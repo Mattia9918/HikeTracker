2449e95aad905e4994e1144ed73e82a3bae589b1
Giorgio <s296395@studenti.polito.it>
2022-12-11 13:16:57 +0100

    S202207HT-129 Front-end development: map management

---
+++ server/test.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO point
+VALUES(26,46.262829,11.868372,"parking","citta","provincia"); 
+
+INSERT INTO point
+VALUES(27,46.272346,11.878534,"parking","citta","provincia"); 
+
+INSERT INTO point
+VALUES(28,46.267836,11.857332,"parking","citta","provincia"); 
+
+
+INSERT INTO parking_lot
+VALUES(1,"parking10_1",0,1,12,0,"10:00-11:00",26); 
+
+
+INSERT INTO parking_lot
+VALUES(2,"parking10_2",0,1,12,0,"10:00-11:00",27); 
+
+
+INSERT INTO parking_lot
+VALUES(3,"parking10_3",0,1,12,0,"10:00-11:00",28); 
